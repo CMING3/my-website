a4766351bc78eba0ad8af895b8370116359f3100
CMING <cming.pms@gmail.com>
2023-02-17 10:48:39 +0800

    created blank page

--- src/components/contents/AboutMe.js
+++ src/components/contents/AboutMe.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import style from '../../styles/aboutme.module.css'
 export default function AboutMe() {
   return (
-    <div>I am CMING</div>
+    <div className={style.aboutMe}>
+      <div className={style.statusBoard}>
+        
+      </div>
+    </div>
   )
 }
