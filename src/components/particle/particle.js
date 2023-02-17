2986d238c5be0a0fd42878734a6237e31ce91ea4
CMING <cming.pms@gmail.com>
2023-02-17 10:41:41 +0800

    created particle background for home page

---
+++ src/components/particle/particle.js
@@ -0,0 +1,73 @@
+import { useCallback } from "react";
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+
+const ParticlesPage = () => {
+
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return(
+        <div>
+            <Particles init={particlesInit} loaded={particlesLoaded} params={{
+                background:{
+                    color:"#000000"
+                },
+                fps_limit: 60,
+                interactivity: {
+                    detectsOn: "parent",
+                    events: {
+                        resize: true,
+                    },
+                },
+                particles:{
+                    color: {
+                        value: "9fafca",
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 1080,
+                        },
+                        limit: 0, 
+                        value: 400,
+                    },
+                    opacity: {
+                        animation: {
+                            enable: true,
+                            minimumValue: 0.05,
+                            speed: 1,
+                            sync: false,
+                        },
+                        random: {
+                            enable: true,
+                            minimumValue: 0.05,
+                        },
+                        value: 1,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: {
+                            enable: true,
+                            minimumValue: 0.5,
+                        },
+                        value: 1,
+                    }
+                }
+            }}/>
+        </div>
+    )
+}
+
+export default ParticlesPage;
