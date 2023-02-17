054964ac03433de6c225d1eb6c0f25ebb19671a9
CMING <cming.pms@gmail.com>
2023-02-17 10:43:47 +0800

    imported contentswitch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "./components/UI/Navbar"
 import Footer from "./components/UI/Footer"
-import Content from "./components/contents/Content"
+import ContentSwitch from "./components/contents/ContentSwitch"
 import {BrowserRouter} from "react-router-dom"
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Navbar/>
           {/** Changeable content, by default is home **/}
-          <Content/>
+          <ContentSwitch/>
         <Footer/>
       </BrowserRouter>
     </div>
