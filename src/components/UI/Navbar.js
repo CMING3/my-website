bd034d5782fb8c9e54125c0b9926323bde55d668
CMING <cming.pms@gmail.com>
2023-02-17 10:45:02 +0800

    completed footer and navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -2,14 +2,19 @@ import style from "../../styles/navbar.module.css"
 import React from "react"
 import {Link} from "react-router-dom"
 export default function Navbar(){
-    const navbarList = ["Home", "AboutMe", "MyProjects", "MyHobbies"]
+    const navbarList = ["Home", "About Me", "My Projects", "My Hobbies"]
 
     return(
         <div className={style.navbar}>
             {
                 navbarList.map((item)=>{
+                    if (item === "Home"){
+                        return(
+                            <Link to="/">{item}</Link>
+                        )
+                    }
                     return(
-                        <Link to={"/"+item}>{item}</Link>
+                        <Link to={"/"+item.replace(/\s+/g, "")}>{item}</Link>
                     )
                 })
             }
