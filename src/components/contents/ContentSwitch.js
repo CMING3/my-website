2be6352a890cca83722192ee0f32e6c7e1bd501b
CMING <cming.pms@gmail.com>
2023-02-17 10:40:17 +0800

    added switch to switch between different pages

---
+++ src/components/contents/ContentSwitch.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from './Home'
+import AboutMe from './AboutMe'
+import MyProjects from './MyProjects'
+import MyHobbies from './MyHobbies'
+
+export default function Content() {
+
+  return (
+      <Routes>
+        <Route  path={"/"} element={<Home/>}/>
+        <Route path="/AboutMe" element={<AboutMe/>}/>
+        <Route path="/MyProjects" element={<MyProjects/>}/>
+        <Route path="/MyHobbies" element={<MyHobbies/>}/>
+      </Routes>
+  )
+}
+
