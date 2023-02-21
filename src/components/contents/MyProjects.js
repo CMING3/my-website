79d06473733c3926114d8a4b2b44f7a34eff1033
CMING <cming.pms@gmail.com>
2023-02-21 16:42:30 +0800

    linked style module

--- src/components/contents/MyProjects.js
+++ src/components/contents/MyProjects.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Style from '../../styles/myproject.module.css'
 
 export default function MyProjects() {
   return (
-    <div>All Projects</div>
+    <div>
+      All Projects
+      <div className={Style.projectList}>
+
+      </div>
+    </div>
   )
 }
